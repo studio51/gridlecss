38ee2c289af28e56d4304271cebdd8559e17d1f0
Vlad Radulescu <pacMakaveli90@gmail.co.uk>
2015-08-13 10:04:24 +0100

    add bower package

---
+++ web/bower_components/gridlecss/Gruntfile.js
@@ -0,0 +1,97 @@
+module.exports = function(grunt) {
+
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    clean: {
+      options: {
+        dot: true
+      },
+
+      dist: {
+        src: ['dist/']
+      }
+    },
+
+    sass: {
+      options: {
+        sourceMap: true
+      },
+
+      dist: {
+        files: {
+          'dist/gridle.css': 'grid/gridlecss.scss'
+        }
+      }
+    },
+
+    postcss: {
+      options: {
+        map: false,
+        processors: [
+          require('autoprefixer-core')({
+            browsers: ['last 5 versions', '> 15%', 'IE 10']
+          }).postcss
+        ]
+      },
+
+      dist: {
+        src: ['dist/gridle.css']
+      }
+    },
+
+    cssnext: {
+      options: {
+        sourcemap: false
+      },
+
+      dist: {
+        files: {
+          'dist/gridle.css': 'dist/gridle.css'
+        }
+      }
+    },
+
+    cssbeautifier : {
+      files : ['dist/gridle.css'],
+      options : {
+        indent: '  ',
+        openbrace: 'end-of-line',
+        autosemicolon: true
+      }
+    },
+
+    cssmin: {
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1
+      },
+
+      target: {
+        files: {
+          'dist/gridle.min.css': ['dist/gridle.css']
+        }
+      }
+    }
+  });
+
+  // Compiles the GridleCSS.css files from source
+  //
+  grunt.registerTask('compile', [
+    'sass',
+    'postcss',
+    'cssnext'
+  ]);
+
+  // Prettifies the css files and creates a minified version
+  //
+  grunt.registerTask('prettify', [
+    'cssbeautifier',
+    'cssmin'
+  ]);
+
+  grunt.registerTask('default', ['clean', 'compile']);
+  grunt.registerTask('ship', ['clean', 'compile', 'prettify']);
+}
---
+++ web/bower_components/gridlecss/web/js/main.js
@@ -0,0 +1,127 @@
+$(document).ready(function() {
+
+  $('.nav-menu .btn').on('click', function(e) {
+    e.preventDefault();
+
+    var target = $(this).data('target');
+
+    $('html, body').stop().animate({
+       scrollTop: $(target).offset().top
+    }, 1000);
+  });
+
+  $('.show-css').on('click', function(e) {
+    e.preventDefault();
+
+    var target = $(this).attr('href'),
+        parent = $('section' + target);
+
+    parent.toggleClass('css');
+  });
+
+  // var stickyNavTop = $('.main').offset().top;
+
+  // var stickyNav = function(){
+  //   var scrollTop = $(window).scrollTop();
+
+  //   if (scrollTop > stickyNavTop) {
+  //     $('.nav-menu').addClass('sticky');
+  //   } else {
+  //     $('.nav-menu').removeClass('sticky');
+  //   }
+  // };
+
+  // stickyNav();
+
+  // $(window).scroll(function() {
+  //     stickyNav();
+  // });
+
+  $('.social-buttons--container ul').sharegg({
+    data: {
+      url: 'https://studio51.github.io/gridlecss',
+      title: 'GridleCSS, yet another flexbox grid!'
+    },
+    buttons: {
+      facebook: {
+        show: true,
+        count: true
+      },
+      googleplus: {
+        show: true,
+        count: true
+      },
+      twitter: {
+        show: true,
+        count: true
+      },
+      pinterest: {
+        show: true,
+        count: true
+      },
+      digg: {
+        show: true,
+        count: true
+      },
+      reddit: {
+        show: true,
+        count: true
+      },
+    }
+  });
+
+  /* open folding content */
+  $('#toggle-menu').on('click', function(event){
+    event.preventDefault();
+    openItemInfo($(this).attr('href'));
+    var menu = $('.nav-menu');
+
+    $(menu).toggleClass('open');
+    $(this).toggleClass('close');
+
+  });
+
+  /* close folding content */
+  $('.cd-folding-panel .cd-close').on('click', function(event){
+    event.preventDefault();
+    toggleContent('', false);
+  });
+  $('.cd-gallery').on('click', function(event){
+    /* detect click on .cd-gallery::before when the .cd-folding-panel is open */
+    if($(event.target).is('.cd-gallery') && $('.fold-is-open').length > 0 ) toggleContent('', false);
+  })
+
+  function openItemInfo(url) {
+      toggleContent(url, true);
+  }
+
+  function toggleContent(url, bool) {
+    if( bool ) {
+      /* load and show new content */
+      var foldingContent = $('.cd-fold-content');
+      foldingContent.load(url+' .cd-fold-content > *', function(event){
+        setTimeout(function(){
+          $('body').addClass('overflow-hidden');
+          $('.cd-folding-panel').addClass('is-open');
+          $('.cd-main').addClass('fold-is-open');
+        }, 100);
+
+      });
+    } else {
+      /* close the folding panel */
+      var mq = viewportSize();
+      $('.cd-folding-panel').removeClass('is-open');
+      $('.cd-main').removeClass('fold-is-open');
+
+      (mq == 'mobile' || $('.no-csstransitions').length > 0 )
+        /* according to the mq, immediately remove the .overflow-hidden or wait for the end of the animation */
+        ? $('body').removeClass('overflow-hidden')
+
+        : $('.cd-main').find('.cd-item').eq(0).one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+          $('body').removeClass('overflow-hidden');
+          $('.cd-main').find('.cd-item').eq(0).off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+        });
+    }
+
+  }
+});
